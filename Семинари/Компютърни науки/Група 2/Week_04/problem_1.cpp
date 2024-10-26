5de64ef2e6face87e284cc396eecaa51582969c0
Boris Vassilev <boris.n.vassilev@gmail.com>
2024-10-26 11:43:35 +0300

    КН Семинар 2 Седмица 4

---
+++ Семинари/Компютърни науки/Група 2/Week_04/problem_1.cpp
@@ -0,0 +1,40 @@
+#include <ios>
+#include <iostream>
+#include <stack>
+#include <stdexcept>
+#include "../Week_03/BoolVector-seminar.hpp"
+
+using BraceType = char;
+
+BraceType getBraceType(const char c) {
+	switch (c) {
+		case '{':
+		case '}': return 0;
+		case '(':
+		case ')': return 1;
+
+		case '<':
+		case '>': return 2;
+		case '[':
+		case ']': return 3;
+		default: throw std::invalid_argument("argument is not a brace");
+	}
+}
+bool isOpenBrace(char c) { return c == '(' || c == '{'; }
+bool isClosingBrace(char c) { return c == ')' || c == '}'; }
+
+bool checkBraces(const std::string &str) {
+	std::stack<bool, BoolVector> stack;
+	for (char c : str) {
+		if (isOpenBrace(c)) { stack.push(getBraceType(c)); }
+		if (isClosingBrace(c)) {
+			if (!stack.empty() && stack.top() == getBraceType(c)) { stack.pop(); }
+		}
+	}
+	return stack.empty();
+}
+
+int main() {
+	std::cout << std::boolalpha << checkBraces("{({}({}))()}") << std::endl;
+	std::cout << std::boolalpha << checkBraces("{({}({})()}") << std::endl;
+}
